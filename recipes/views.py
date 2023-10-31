c6bedcd17a9c7f7681afdfd4b0a4dafcc5682098
AyushGupta7005 <12321ayushgupta@gmail.com>
2023-10-31 22:38:57 +0530

    CRUD operartions applied

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,49 @@
 from django.shortcuts import render,redirect
 from .models import *
+from django.contrib.auth.models import User 
 # Create your views here.
+
 def index(request):
     if request.method=="POST":
         data=request.POST
         Recipe.objects.create(
             name=data.get('name'),
-            description=data.get('description')
+            description=data.get('description'),
+            user=request.user
         )
-        return redirect('/Recipe/')
+        return redirect('homepage')
     return render(request,'index.html')
+
+
+def home(request):
+    receipe_list=Recipe.objects.all()
+    context={'recipe':receipe_list}
+    return render(request,'home.html',context)
+
+
+def myrecipes(request):
+    receipe_list=Recipe.objects.filter(user=request.user)
+    context={'recipe':receipe_list}
+    return render(request,'myrecipes.html',context)
+
+
+def delete_recipe(request,id):
+    queryset=Recipe.objects.get(id=id)
+    queryset.delete()
+    return redirect('myrecipes')
+
+def update_recipe(request,id):
+    queryset=Recipe.objects.get(id=id)
+    description=queryset.description
+    name=queryset.name
+
+    if request.method=="POST":
+        data=request.POST
+        name=data.get('name')
+        description=data.get('description')
+        queryset.description=description
+        queryset.name=name
+        queryset.save()
+        return redirect('myrecipes')
+    context={'recipe':queryset}
+    return render(request,'update_recipe.html',context)
